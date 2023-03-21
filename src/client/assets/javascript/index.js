4bea98cfa1e757559eb0cd31ed7a0fc1f2329902
Shimon Moyal <shimon.m@minute.ly>
2023-03-21 08:51:23 +0200

    moved renderCanvas to a new file

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -1,7 +1,8 @@
+import {renderCanvas} from "./drawRace.js"
 // PROVIDED CODE BELOW (LINES 1 - 80) DO NOT REMOVE
 
 // The store will hold all information needed globally
-let store = {
+export let store = {
   track_id: undefined,
   player_id: undefined,
   race_id: undefined,
@@ -411,7 +412,7 @@ function defaultFetchOpts() {
 
 // TODO - Make a fetch call (with error handling!) to each of the following API endpoints
 
-async function getTracks() {
+export async function getTracks() {
   // GET request to `${SERVER}/api/tracks`
   try {
     const tracks = await fetch(`${SERVER}/api/tracks`);
@@ -533,64 +534,72 @@ function checkMissingSelection(player_id, track_id) {
   return missingInfo;
 }
 
+// async function renderCanvas(trackId, positions) {
+//   const track = (await getTracks()).find((t) => t.id === +trackId);
 
-async function renderCanvas(trackId, positions) {
-  const track = (await getTracks()).find((t) => t.id === +trackId);
+//   const canvas = document.getElementById("race-canvas");
+//   const ctx = canvas?.getContext("2d");
 
-  const canvas = document.getElementById("race-canvas");
-  const ctx = canvas?.getContext("2d");
+//   const trackWidth = canvas.width;
+//   const trackHeight = canvas.height;
 
-  const trackWidth = canvas.width;
-  const trackHeight = canvas.height;
+//   ctx.clearRect(0, 0, trackWidth, trackHeight);
 
-  ctx.clearRect(0, 0, trackWidth, trackHeight);
+//   ctx.fillStyle = "#eee";
+//   ctx.fillRect(0, 0, trackWidth, trackHeight);
 
-  ctx.fillStyle = "#eee";
-  ctx.fillRect(0, 0, trackWidth, trackHeight);
+//   const carWidth = 60;
+//   const carHeight = 60;
 
-  const carWidth = 30;
-  const carHeight = 60;
+//   const carColors = ["red", "blue", "green", "yellow", "orange"];
 
-  const carColors = ["red", "blue", "green", "yellow", "pink"];
+//   const carImages = {};
+//   await Promise.all(
+//     carColors.map(async (color) => {
+//       const img = new Image();
+//       img.src = `../assets/cars/${color}car.png`;
+//       await new Promise((resolve) => {
+//         img.onload = resolve;
+//       });
+//       carImages[color] = img;
+//     })
+//   );
 
-  const carImages = {};
-  await Promise.all(
-    carColors.map(async (color) => {
-      const img = new Image();
-      img.src = `../assets/cars/${color}car.png`;
-      await new Promise((resolve) => {
-        img.onload = resolve;
-      });
-      carImages[color] = img;
-    })
-  );
+//   positions.forEach((position) => {
+//     if (store[position.driver_name] === undefined) {
+//       const availableColors = carColors.filter((color) => {
+//         return !Object.values(store).includes(color);
+//       });
+//       store[position.driver_name] =
+//         availableColors[Math.floor(Math.random() * availableColors.length)];
+//     }
+//     position.color = store[position.driver_name];
+//   });
 
-  positions.forEach((position) => {
-    if (store[position.driver_name] === undefined) {
-      const availableColors = carColors.filter((color) => {
-        return !Object.values(store).includes(color);
-      });
-      store[position.driver_name] = availableColors[Math.floor(Math.random() * availableColors.length)];
-    }
-    position.color = store[position.driver_name];
-  });
+//   ctx.strokeStyle = "black";
+//   ctx.lineWidth = 2;
+//   ctx.setLineDash([20, 5]); // Set dashed line pattern
+//   for (let i = 1; i < positions.length; i++) {
+//     const y = i * (carHeight + 10) - 5;
+//     ctx.beginPath();
+//     ctx.moveTo(0, y);
+//     ctx.lineTo(trackWidth, y);
+//     ctx.stroke();
+//   }
+//   ctx.setLineDash([]); // Reset line dash pattern
 
-  positions.forEach((position, index) => {
-    const segmentPercentage = position.segment / track.segments.length;
-    const x = segmentPercentage * trackWidth;
-    const y = (index * (carHeight + 10)) % trackHeight;
+//   positions.forEach((position, index) => {
+//     const segmentPercentage = position.segment / track.segments.length;
+//     const x = segmentPercentage * trackWidth;
+//     const y = (index * (carHeight + 10)) % trackHeight;
 
-    const carImage = carImages[position.color];
-    console.log("carImage: ", position.color);
+//     const carImage = carImages[position.color];
+//     console.log("carImage: ", position.color);
 
-    ctx.drawImage(carImage, x, y, carWidth, carHeight);
+//     ctx.drawImage(carImage, x, y, carWidth, carHeight);
 
-    ctx.fillStyle = "black";
-    ctx.font = "14px Arial";
-    ctx.fillText(position.driver_name, x + carWidth + 5, y + carHeight / 2);
-  });
-}
-
-  
-  
-  
+//     ctx.fillStyle = "black";
+//     ctx.font = "14px Arial";
+//     ctx.fillText(position.driver_name, x + carWidth + 5, y + carHeight / 2);
+//   });
+// }
